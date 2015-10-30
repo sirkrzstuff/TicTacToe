c90af93a1f80d25e36e863fc0c82b7f2596b2dbe
Gudjon Mar <gudjonmar@gmail.com>
2015-10-30 00:41:28 +0000

    Added Tictactoe.java and Tictactoetest.java

---
+++ TicTacToe Project/src/main/java/is/ru/fenrir/tictactoe/Tictactoe.java
@@ -0,0 +1,10 @@
+package main.java.is.ru.fenrir.tictactoe;
+
+public class Tictactoe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TicTacToe Project/src/test/java/is/ru/fenrir/tictactoetest/Tictactoetest.java
@@ -0,0 +1,5 @@
+package test.java.is.ru.fenrir.tictactoetest;
+
+public class Tictactoetest {
+
+}
