93d12ee3976951651563e78c3a6a7ce08b5d3fd9
Jon Bjorn Njalsson <jonbn74@gmail.com>
2015-10-31 19:39:03 +0000

    before troða takka

--- TicTacToe Project/src/main/java/is/ru/fenrir/tictactoe/Graphics.java
+++ TicTacToe Project/src/main/java/is/ru/fenrir/tictactoe/Graphics.java
@@ -18,15 +18,20 @@ public class Graphics extends JFrame {
 		
 		for(int i = 0; i < 9; i++)
 		{
-			buttons[i] = new JButton(" ");
+			buttons[i] = new JButton("");
 			buttons[i].setFont(new Font("Arial", Font.BOLD, 80));
 			panel.add(buttons[i]);
 		}
 		
 		
-		JLabel status = new JLabel("Status");
+		JLabel status = new JLabel("X´s turn");
 		add(panel, BorderLayout.CENTER);
 		add(status, BorderLayout.SOUTH);
-		
+		//Settings
+		this.setSize(500, 500);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setTitle("TicTacToe");
 	}
 }
--- TicTacToe Project/src/main/java/is/ru/fenrir/tictactoe/Tictactoe.java
+++ TicTacToe Project/src/main/java/is/ru/fenrir/tictactoe/Tictactoe.java
@@ -3,15 +3,22 @@ package main.java.is.ru.fenrir.tictactoe;
 import javax.swing.JFrame;
 
 public class Tictactoe {
+	
+	private static boolean playing = true;
 
 	public static void main(String[] args) {
 		JFrame Graphics = new Graphics();
-		Graphics.setSize(500, 500);
-		Graphics.setVisible(true);
-		Graphics.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Graphics.setLocationRelativeTo(null);
-		Graphics.setTitle("TicTacToe");
+		
 
+		while(playing)
+		{
+			
+			
+		}
+	}
+	
+	public void setToken() {
+		
 	}
 	
 
